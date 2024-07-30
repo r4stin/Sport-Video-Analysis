e2174e7f96debc6f3cc46279194fc84ef7151871
Rastin <r.akbari625@gmail.com>
2024-07-30 15:04:31 +0200

    header updated

--- include/BallDetection.h
+++ include/BallDetection.h
@@ -1,5 +1,5 @@
-#ifndef CV_FINAL_BALLDETECTION_H
-#define CV_FINAL_BALLDETECTION_H
+#ifndef BALLDETECTION_H
+#define BALLDETECTION_H
 #include "header.h"
 
 class BallDetection {
@@ -7,15 +7,16 @@ class BallDetection {
 public:
     BallDetection();
     cv::Mat removePixel(cv::Mat img, int rmp);
+    bool processTableObjects(const cv::Mat& frame, const cv::Rect& roiRect);
+    cv::Mat create_table(int width, int height);
     cv::Mat draw_balls( const std::vector<cv::Point2f>& minimapBallPositions, const cv::Mat& background, int radius, int size, const cv::Mat& img);
     cv::Mat draw_holes(const cv::Mat& input_img);
-    cv::Mat create_table(int width, int height);
     bool createTopViewMinimap(const std::vector<cv::Point2f>& ballPositions, const cv::Mat& img, const std::vector<cv::Point2f>& tableCorners);
-    bool process_video(const std::string& input_path,const std::string& output_path);
-    bool processTableObjects(const cv::Mat& frame, const cv::Rect& roiRect);
     bool outputGenerator(const std::vector<cv::Point2f>& ballPositions, const cv::Mat& img, int radius, const cv::Mat& mask_table, const cv::Mat& bb_table, const std::string& filename);
     static void saveDetections(const std::string& filename, const std::vector<cv::Point2f>& centers, const std::vector<int>& labels, const std::vector<cv::Rect>& boundingBoxes);
     bool centerRefinement(cv::Mat img);
+    bool process_video(const std::string& input_path,const std::string& output_path);
+
 
 
 
@@ -25,20 +26,23 @@ public:
 
 private:
     cv::VideoCapture capture_;
+    cv::Mat top_view_;
+    std::vector<cv::Point2f> centers_;
+    std::vector<cv::Point2f> centers_ref_;
+    std::vector<float> radius_;
+    std::vector<cv::Point2f> points_;
+    cv::Point2f transformPoint(const cv::Point2f& point, const cv::Mat& transformMatrix);
+
+
+
     int width_ = 400;
     int height_ = 800;
 
 
-    cv::Mat top_view_;
-    std::vector<cv::Point2f> centers_;
-    std::vector<cv::Point2f> centers_ref_;
-    std::vector<cv::Point2f> points_;
-    std::vector<float> radius_;
-    cv::Point2f transformPoint(const cv::Point2f& point, const cv::Mat& transformMatrix);
 
 
-        friend class TableDetection;
-        friend class findCenters;
+    friend class TableDetection;
+    friend class findCenters;
 
 };
 
@@ -62,7 +66,6 @@ private:
 class findCenters {
 public:
     explicit findCenters(BallDetection *ballDetection1);
-    std::vector<cv::Point2f> findCenter(cv::Mat img);
 
 
 
@@ -73,4 +76,4 @@ private:
 };
 
 
-#endif //CV_FINAL_BALLDETECTION_H
+#endif //BALLDETECTION_H
